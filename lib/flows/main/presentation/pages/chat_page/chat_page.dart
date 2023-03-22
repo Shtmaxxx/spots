71710f95ac9c1ab7861dffb1028061b335a90ba5
Maksym Shtoler <maksym.shtoler@mobindustry.net>
2023-03-23 00:31:26 +0200

    iOS setup
    Changelog: added

--- lib/flows/main/presentation/pages/chat_page/chat_page.dart
+++ lib/flows/main/presentation/pages/chat_page/chat_page.dart
@@ -37,101 +37,102 @@ class ChatPage extends StatelessWidget {
             );
           }
         },
-        child: Builder(
-          builder: (context) {
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Maksym Shtoler'),
-                centerTitle: true,
-              ),
-              backgroundColor: Theme.of(context).canvasColor,
-              body: Stack(
-                children: [
-                  BlocBuilder<MessagesCubit, MessagesState>(
-                    builder: (context, state) {
-                      if (state is MessagesInitial) {
-                        return StreamBuilder<List<Message>>(
-                          stream: state.stream,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasError) {
-                              context.read<MessagesCubit>().emitError(
-                                    'Error: ${snapshot.error.toString()}',
-                                  );
-                            } else if (snapshot.hasData) {
-                              final messages = snapshot.data!;
+        child: Builder(builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Maksym Shtoler'),
+              centerTitle: true,
+            ),
+            backgroundColor: Theme.of(context).canvasColor,
+            body: Stack(
+              children: [
+                BlocBuilder<MessagesCubit, MessagesState>(
+                  builder: (context, state) {
+                    if (state is MessagesInitial) {
+                      return StreamBuilder<List<Message>>(
+                        stream: state.stream,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            context.read<MessagesCubit>().emitError(
+                                  'Error: ${snapshot.error.toString()}',
+                                );
+                          } else if (snapshot.hasData) {
+                            final messages = snapshot.data!;
 
-                              return SingleChildScrollView(
-                                reverse: true,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  verticalDirection: VerticalDirection.up,
-                                  children: [
-                                    const SizedBox(height: 55),
-                                    ...messages
-                                        .map(
-                                          (item) => MessageItem(
-                                            message: item.messageText,
-                                            time: DateFormat.Hm()
-                                                .format(item.dateTime),
-                                            sentByUser: item.sentByUser,
-                                          ),
-                                        )
-                                        .toList(),
-                                  ],
-                                ),
-                              );
-                            }
-                            return const CircularLoading();
-                          },
-                        );
-                      }
-                      return Container();
-                    },
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      height: 50,
-                      decoration: const BoxDecoration(
-                        color: Color(0xffdcddfa),
-                      ),
-                      padding: const EdgeInsets.only(left: 20, right: 15),
-                      child: Row(
-                        children: [
-                          MessageTextField(
-                            controller:
-                                context.read<MessagesCubit>().messageController,
-                          ),
-                          const SizedBox(width: 10),
-                          TextButton(
-                            onPressed: () async =>
-                                await context.read<MessagesCubit>().sendMessage(
-                                      chatId: chatId,
-                                      senderId: user.id,
-                                      dateTime: DateTime.now(),
-                                    ),
-                            style: TextButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor,
-                              padding: const EdgeInsets.all(6),
-                              minimumSize: const Size(0, 0),
-                            ),
-                            child: const Text(
-                              'Send',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
+                            return SingleChildScrollView(
+                              reverse: true,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                verticalDirection: VerticalDirection.up,
+                                children: [
+                                  const SizedBox(height: 55),
+                                  ...messages
+                                      .map(
+                                        (item) => MessageItem(
+                                          message: item.messageText,
+                                          time: DateFormat.Hm()
+                                              .format(item.dateTime),
+                                          sentByUser: item.sentByUser,
+                                        ),
+                                      )
+                                      .toList(),
+                                ],
                               ),
+                            );
+                          }
+                          return const CircularLoading();
+                        },
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 90,
+                    decoration: const BoxDecoration(
+                      color: Color(0xffdcddfa),
+                    ),
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 10, right: 15),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        MessageTextField(
+                          controller:
+                              context.read<MessagesCubit>().messageController,
+                        ),
+                        const SizedBox(width: 10),
+                        TextButton(
+                          onPressed: () async =>
+                              await context.read<MessagesCubit>().sendMessage(
+                                    chatId: chatId,
+                                    senderId: user.id,
+                                    dateTime: DateTime.now(),
+                                  ),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Theme.of(context).primaryColor,
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            padding: const EdgeInsets.all(6),
+                            minimumSize: const Size(0, 0),
+                          ),
+                          child: const Text(
+                            'Send',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            );
-          }
-        ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
